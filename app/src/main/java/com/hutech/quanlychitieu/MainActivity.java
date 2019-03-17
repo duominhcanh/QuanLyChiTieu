b6734c940fc186c9648aac9a37c68432d3b1a893
Dương Minh Cảnh <39360747+duominhcanh@users.noreply.github.com>
2019-03-17 19:19:25 +0700

    Thêm list item cho trang chủ

--- app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
+++ app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
@@ -1,11 +1,14 @@
 package com.hutech.quanlychitieu;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +16,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(data!= null){
+            Bundle bdl= data.getExtras();
+            Toast.makeText(this, ""+bdl.getInt("test"), Toast.LENGTH_SHORT).show();
+        }
     }
 
     //Menu cài đặt
--- app/src/main/java/com/hutech/quanlychitieu/MainSettingActivity.java
+++ app/src/main/java/com/hutech/quanlychitieu/MainSettingActivity.java
@@ -1,7 +1,9 @@
 package com.hutech.quanlychitieu;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainSettingActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainSettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_setting);
+    }
+
+    public void btnCancelClicked(View view) {
+        finish();
+    }
+
+    public void btnAcceptClicked(View view) {
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+
+        bundle.putInt("test", 1);
+
+        intent.putExtras(bundle);
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
