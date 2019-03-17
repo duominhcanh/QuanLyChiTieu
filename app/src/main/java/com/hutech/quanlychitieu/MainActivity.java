95d1006691a978063668cc9d414b77f414d2e245
Dương Minh Cảnh <39360747+duominhcanh@users.noreply.github.com>
2019-03-17 18:42:08 +0700

    Lam mot xiu

--- app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
+++ app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
@@ -2,6 +2,9 @@ package com.hutech.quanlychitieu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Menu cài đặt
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.maintabmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch(item.getItemId()){
+            case R.id.menuSettings:
+                break;
+
+        }
+        return true;
     }
 }
