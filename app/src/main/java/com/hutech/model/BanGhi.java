fcf02d0f46f89e0655cdc3e174d68720d4f6c509
Dương Minh Cảnh <39360747+duominhcanh@users.noreply.github.com>
2019-03-17 19:52:52 +0700

    Tao model Ban Ghi

--- app/src/main/java/com/hutech/model/BanGhi.java
+++ app/src/main/java/com/hutech/model/BanGhi.java
@@ -1,5 +1,60 @@
 package com.hutech.model;
 
 public class BanGhi {
-    
+    private int ma;
+    private String noiDung;
+    private String thoiGian;
+    private String soTien;
+    private String maDongTien;
+
+    public BanGhi(int ma, String noiDung, String thoiGian, String soTien, String maDongTien) {
+        this.ma = ma;
+        this.noiDung = noiDung;
+        this.thoiGian = thoiGian;
+        this.soTien = soTien;
+        this.maDongTien = maDongTien;
+    }
+
+    public BanGhi() {
+    }
+
+    public int getMa() {
+        return ma;
+    }
+
+    public void setMa(int ma) {
+        this.ma = ma;
+    }
+
+    public String getNoiDung() {
+        return noiDung;
+    }
+
+    public void setNoiDung(String noiDung) {
+        this.noiDung = noiDung;
+    }
+
+    public String getThoiGian() {
+        return thoiGian;
+    }
+
+    public void setThoiGian(String thoiGian) {
+        this.thoiGian = thoiGian;
+    }
+
+    public String getSoTien() {
+        return soTien;
+    }
+
+    public void setSoTien(String soTien) {
+        this.soTien = soTien;
+    }
+
+    public String getMaDongTien() {
+        return maDongTien;
+    }
+
+    public void setMaDongTien(String maDongTien) {
+        this.maDongTien = maDongTien;
+    }
 }
