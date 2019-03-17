58c253d2ef3f3ac7d7918cedf25130a8750e1beb
Dương Minh Cảnh <39360747+duominhcanh@users.noreply.github.com>
2019-03-17 20:47:42 +0700

    Datas

--- app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
+++ app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hutech.quanlychitieu;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,13 +12,23 @@ import android.view.MenuItem;
 import android.widget.TabHost;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity {
+    String DATABASE_NAME="quanlychitieu.db";
+    String DB_PATH_SUFFIX = "/databases/";
+    SQLiteDatabase database=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addControls();
+        processCopy();
     }
 
     private void addControls() {
@@ -80,5 +91,53 @@ public class MainActivity extends AppCompatActivity {
         Intent i= new Intent(this, MainSettingActivity.class);
         final int result=1;
         startActivityForResult(i, result);
+    }
+
+
+    //Database
+    private void processCopy() {
+        File dbFile = getDatabasePath(DATABASE_NAME);
+        if (!dbFile.exists())
+        {
+            try
+            {
+                CopyDataBaseFromAsset();
+                Toast.makeText(this,
+                        "Copying sucess from Assets folder",
+                        Toast.LENGTH_LONG).show();
+            }
+            catch (Exception e)
+            {
+                Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private String getDatabasePath() {
+        return getApplicationInfo().dataDir + DB_PATH_SUFFIX+ DATABASE_NAME;
+    }
+
+    public void CopyDataBaseFromAsset()
+    {
+        try {
+            InputStream myInput;
+            myInput = getAssets().open(DATABASE_NAME);
+            String outFileName = getDatabasePath();
+            File f = new File(getApplicationInfo().dataDir + DB_PATH_SUFFIX);
+            if (!f.exists())
+                f.mkdir();
+            OutputStream myOutput = new FileOutputStream(outFileName);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = myInput.read(buffer)) > 0) {
+                myOutput.write(buffer, 0, length);
+            }
+            myOutput.flush();
+            myOutput.close();
+            myInput.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
