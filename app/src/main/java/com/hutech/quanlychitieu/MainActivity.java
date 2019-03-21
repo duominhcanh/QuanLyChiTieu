960d81a52688ea95133a90146fc9591b3db7948c
Dương Minh Cảnh <39360747+duominhcanh@users.noreply.github.com>
2019-03-21 15:20:03 +0700

    Sửa lỗi
    Không biết tại sao lỗi luôn :))

--- app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
+++ app/src/main/java/com/hutech/quanlychitieu/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hutech.quanlychitieu;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.support.annotation.Nullable;
@@ -10,8 +11,13 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TabHost;
 import android.widget.Toast;
+
+import com.hutech.adapter.BanGhiAdapter;
+import com.hutech.model.BanGhi;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -20,44 +26,92 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Các biến liên quan đến database
     String DATABASE_NAME="quanlychitieu.db";
     String DB_PATH_SUFFIX = "/databases/";
     SQLiteDatabase database=null;
+
+    //Khai báo các adapters
+    BanGhiAdapter banGhiAdapter;
+
+    //Các biến controls
+    Button btnAddRecord;
+
+    ListView lvTab1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Chạy hàm thêm database
+        processCopy();
+
+        //Khởi tạo các biến, ví dụ Adapter
+        initProps();
+
+        //Load controls từ file layout
         addControls();
 
-        processCopy();
+        //Liên kết các adapter với controls
+        linkAdapterToControls();
+    }
+
+    private void linkAdapterToControls() {
+        /**
+         * Liên kết các adapter với controls
+         */
+        lvTab1.setAdapter(banGhiAdapter);
+    }
+
+    private void initProps() {
+        /**
+         * Khởi tạo các thuộc tính trong class
+         * Đã làm: [banGhiAdapter]
+         */
+        banGhiAdapter= new BanGhiAdapter(MainActivity.this, R.layout.mainlistitem);
+        loadBanGhi();
     }
 
     private void addControls() {
+        // Tab layout START <<<<<<<<<<<<<
         TabHost host = (TabHost) findViewById(R.id.tabHost);
         host.setup();
-        TabHost.TabSpec spec = host.newTabSpec("Tab One");
+        TabHost.TabSpec spec = host.newTabSpec("Tab One").setIndicator("",getResources().getDrawable(R.drawable.record));
         spec.setContent(R.id.tab1);
         spec.setIndicator("Tab One");
+        spec.setIndicator("",getResources().getDrawable(R.drawable.record));
         host.addTab(spec);
         //Tab 2
-        spec = host.newTabSpec("Tab Two");
+        spec = host.newTabSpec("Tab Two").setIndicator("",getResources().getDrawable(R.drawable.stat));
         spec.setContent(R.id.tab2);
         spec.setIndicator("Tab Two");
+        spec.setIndicator("",getResources().getDrawable(R.drawable.stat));
         host.addTab(spec);
-
         //Tab 3
-        spec = host.newTabSpec("Tab Three");
+        spec = host.newTabSpec("Tab Three").setIndicator("",getResources().getDrawable(R.drawable.category));
         spec.setContent(R.id.tab3);
         spec.setIndicator("Tab Three");
+        spec.setIndicator("",getResources().getDrawable(R.drawable.category));
         host.addTab(spec);
         //Tab 4
-        spec = host.newTabSpec("Tab Four");
+        spec = host.newTabSpec("Tab Four").setIndicator("",getResources().getDrawable(R.drawable.info));
         spec.setContent(R.id.tab4);
         spec.setIndicator("Tab Four");
+        spec.setIndicator("",getResources().getDrawable(R.drawable.info));
         host.addTab(spec);
+
+        // Tab layout END <<<<<<<<<<<<<
+
+        // Load controls trên tab 1
+        btnAddRecord= findViewById(R.id.btnAddRecord);
+        lvTab1= findViewById(R.id.lvTab1);
+
     }
 
+
+    //Option menu Start <<<<<<<<<<<<<<<<<
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -66,8 +120,6 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, ""+bdl.getInt("test"), Toast.LENGTH_SHORT).show();
         }
     }
-
-    //Menu cài đặt
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -95,8 +147,9 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(i, result);
     }
 
+    //Option menu END <<<<<<<<<<<<<<<<<
 
-    //Database
+    //Database START <<<<<<<<<<<<<<<<<<<<
     private void processCopy() {
         File dbFile = getDatabasePath(DATABASE_NAME);
         if (!dbFile.exists())
@@ -143,5 +196,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //Database END <<<<<<<<<<<<<<<<<<<<
 
+    //Thao tác adapter : lấy dữ liệu
+
+    private void loadBanGhi(){
+        /**
+         * Load danh sách các bản ghi vào [banGhiAdapter]
+         */
+        database= openOrCreateDatabase(DATABASE_NAME, MODE_PRIVATE, null);
+        Cursor cursor= database.rawQuery(
+                "select b.ma, b.noiDung,  b.thoiGian, b.soTien, b.maHoatDong, h.tenHoatDong, h.maDongTien, d.tenDong " +
+                        "from BanGhi b, DongTien d, HoatDong h " +
+                        "where b.maHoatDong= h.maHoatDong and h.maDongTien= d.maDong", null);
+        while(cursor.moveToNext()){
+            int    ma=             cursor.getInt(0);
+            String noiDung=     cursor.getString(1);
+            String thoiGian=    cursor.getString(2);
+            int    soTien=         cursor.getInt(3);
+            int    maHoatDong=     cursor.getInt(4);
+            String tenHoatDong= cursor.getString(5);
+            int    maDongTien=     cursor.getInt(6);
+            String tenDong=     cursor.getString(7);
+
+            BanGhi banGhi= new BanGhi(ma, noiDung, thoiGian,
+                    soTien, maHoatDong, tenHoatDong, maDongTien, tenDong);
+            banGhiAdapter.add(banGhi);
+        }
+    }
 }
